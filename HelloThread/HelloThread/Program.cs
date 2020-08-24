2f5dd0739816e26158f682bf4dad22aa10e1007f
TheLittleCitizen9 <thelittlecitizen9@gmail.com>
2020-08-24 20:23:28 +0300

    Finished section 2

---
+++ HelloThread/HelloThread/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace HelloThread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                int threadNum = i +1;
+                if(threadNum == 1)
+                {
+                    Thread thread = new Thread(() => Print(threadNum, "Mamas"));
+                    thread.Start();
+                }
+                else
+                {
+                    Thread thread = new Thread(() => Print(threadNum, "Empire"));
+                    thread.Start();
+                }
+                    
+            }
+            
+        }
+
+        public static void Print(int threadNum, string toPrint)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine($"thread: {threadNum} - {toPrint}");
+            }
+        }
+    }
+}
